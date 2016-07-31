3d6ba14e33d2c43d8c5af62e5a31e7c2680f27f9
ykichanov <ykichanov@ya.ru>
2016-07-31 21:43:06 +0400

    Update i2p to actual 0.9.26 version

--- Formula/i2p.rb
+++ Formula/i2p.rb
@@ -1,12 +1,12 @@
 class I2p < Formula
   desc "I2P is an anonymous overlay network - a network within a network"
   homepage "https://geti2p.net"
-  url "https://download.i2p2.de/releases/0.9.21/i2pinstall_0.9.21.jar"
-  sha256 "0238ffc6ea44099ef4fe6c20913cb4eec675c2760aea07dfe7d499addcc89cf2"
+  url "https://download.i2p2.de/releases/0.9.26/i2pinstall_0.9.26.jar"
+  sha256 "563eb6f2cb9220c380190e90290cd154da3f30b4fa96a212a80e4bbc7a8fd44f"
 
   bottle :unneeded
 
-  depends_on :java => "1.6+"
+  depends_on :java => "1.7+"
 
   def install
     (buildpath/"path.conf").write "INSTALL_PATH=#{libexec}"
